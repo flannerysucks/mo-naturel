fe63a048fe9641ca8e65b23b6412f80abeec61c7
flannerysucks <142702780+flannerysucks@users.noreply.github.com>
2023-08-20 15:05:53 -0500

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,36 +1,41 @@
 import * as fs from 'fs';
-export default async function getPostText() {
+export default async function getPostText(): Promise<string> {
+  return new Promise<string>((resolve, reject) => {
+    const calendarFilePath = 'calendar.txt';
+    let lines: string[] = [];
+    let currentIndex = 0;
 
-  const calendarFilePath = 'calendar.txt';
-  let lines: string[] = [];
-  let currentIndex = 0;
-  
-  // Read the lines from the calendar.txt file
-  try {
-    const data = fs.readFileSync(calendarFilePath, 'utf-8');
-    lines = data.split('\n').filter(line => line.trim() !== '');
-  } catch (error) {
-    console.error('Error reading the calendar file:', error);
-    process.exit(1);
-  }
-  
-  // Function to post a line and schedule the next post
-  function postLine() {
-    if (currentIndex < lines.length) {
-      console.log(lines[currentIndex]);
-      currentIndex++;
-      scheduleNextPost();
-    } else {
-      console.log('All lines have been posted.');
+    // Read the lines from the calendar.txt file
+    try {
+      const data = fs.readFileSync(calendarFilePath, 'utf-8');
+      lines = data.split('\n').filter(line => line.trim() !== '');
+    } catch (error) {
+      console.error('Error reading the calendar file:', error);
+      reject(error); // Reject the promise with the error
+      return;
     }
-  }
-  
-  // Function to schedule the next post
-  function scheduleNextPost() {
-    const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
-    setTimeout(postLine, oneDayInMilliseconds);
-  }
-  
-  // Start posting
-  return "hello";
+
+    // Function to post a line and schedule the next post
+    function postLine() {
+      if (currentIndex < lines.length) {
+        console.log(lines[currentIndex]);
+        currentIndex++;
+        scheduleNextPost();
+      } else {
+        console.log('All lines have been posted.');
+        resolve('All lines have been posted.'); // Resolve the promise
+        // Terminate the Node.js process after all lines have been posted
+      }
+    }
+
+    // Function to schedule the next post
+    function scheduleNextPost() {
+      const oneDayInMilliseconds = 24 * 60 * 60 * 1000; // 1 day in milliseconds
+      setTimeout(postLine, oneDayInMilliseconds);
+    }
+
+    // Start posting
+    postLine(); // Start the posting process
+    process.exit(0);
+  });
 }
