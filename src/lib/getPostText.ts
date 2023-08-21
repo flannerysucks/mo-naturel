dc9e9b64c81c662666323eac83da498b72fde2b7
flannerysucks <142702780+flannerysucks@users.noreply.github.com>
2023-08-20 20:28:07 -0500

    Update getPostText.ts

--- src/lib/getPostText.ts
+++ src/lib/getPostText.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs/promises';
 
-async function getPostText(): Promise<string> {
+export default async function getPostText(): Promise<string> {
     const currentDate = new Date();
     const currentDay = currentDate.getDate();
     
